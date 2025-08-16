c3d833b9b4366ddd25e79ea874aa08d066cefbc6
Yusuf <vizart.id@gmail.com>
2025-08-16 23:06:27 +0700

    Initial commit: Menambahkan file proyek

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-
-const TechIcon = ({ src, name, className = '' }) => {
+import React from "react"; 
+import { motion } from "framer-motion";
+const TechIcon = ({ src, name, className = "" }) => {
   return (
     <img
       src={src}
@@ -15,172 +14,214 @@ const TechIcon = ({ src, name, className = '' }) => {
 const TechStack = () => {
   const techCategories = [
     {
-      category: 'Frontend Development',
+      category: "Full-Stack Development",
       technologies: [
-        { name: 'Next.js', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg', color: 'from-gray-800 to-gray-600' },
-        { name: 'React', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg', color: 'from-blue-400 to-cyan-400' },
-        { name: 'Vue.js', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg', color: 'from-green-400 to-emerald-400' },
-        { name: 'Tailwind CSS', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg', color: 'from-cyan-500 to-blue-500' },
-        { name: 'HTML5', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg', color: 'from-orange-500 to-pink-500' },
-        { name: 'CSS3', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg', color: 'from-blue-500 to-indigo-500' },
-        { name: 'JavaScript', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg', color: 'from-yellow-400 to-yellow-500' },
-        { name: 'TypeScript', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg', color: 'from-blue-600 to-blue-400' },
-        { name: 'Astro', src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/astro/astro-original.svg', color: 'from-purple-600 to-indigo-600' },
-        { name: 'Vite', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vite/vite-original.svg', color: 'from-purple-500 to-yellow-500' },
-      ]
+        {
+          name: "Next.js",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+          color: "from-gray-800 to-gray-600",
+        },
+        {
+          name: "Node.js",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+          color: "from-green-500 to-emerald-400",
+        },
+        {
+          name: "React",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+          color: "from-blue-400 to-cyan-400",
+        },
+        {
+          name: "Vue.js",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
+          color: "from-green-400 to-emerald-400",
+        },
+        {
+          name: "Tailwind CSS",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
+          color: "from-cyan-500 to-blue-500",
+        },
+        {
+          name: "Express.js",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+          color: "from-gray-700 to-gray-500",
+        },
+        {
+          name: "PHP",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
+          color: "from-indigo-500 to-purple-500",
+        },
+        {
+          name: "Python",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+          color: "from-yellow-500 to-blue-500",
+        },
+        {
+          name: "HTML5",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+          color: "from-orange-500 to-pink-500",
+        },
+        {
+          name: "CSS3",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+          color: "from-blue-500 to-indigo-500",
+        },
+        {
+          name: "JavaScript",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+          color: "from-yellow-400 to-yellow-500",
+        },
+        {
+          name: "TypeScript",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+          color: "from-blue-600 to-blue-400",
+        },
+        {
+          name: "Astro",
+          src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/astro/astro-original.svg",
+          color: "from-purple-600 to-indigo-600",
+        },
+        {
+          name: "jupyter",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jupyter/jupyter-original-wordmark.svg",
+          color: "from-orange-600 to-white-600",
+        },
+        {
+          name: "Vite",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vite/vite-original.svg",
+          color: "from-purple-500 to-blue-500",
+        },
+      ],
     },
+
     {
-      category: 'Backend Development',
+      category: "DevOps & Platforms",
       technologies: [
-        { name: 'Node.js', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg', color: 'from-green-500 to-emerald-400' },
-        { name: 'Express.js', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg', color: 'from-gray-700 to-gray-500' },
-        { name: 'PHP', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg', color: 'from-indigo-500 to-purple-500' },
-        { name: 'Python', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg', color: 'from-yellow-500 to-blue-500' },
-        { name: 'FastAPI', src: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/fastapi/fastapi-original.svg', color: 'from-emerald-500 to-teal-500' },
-        { name: 'Laravel', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-original.svg', color: 'from-red-500 to-pink-500' },
-      ]
-    },
-    {
-      category: 'Database & CMS',
-      technologies: [
-        { name: 'MongoDB', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg', color: 'from-green-600 to-green-400' },
-        { name: 'MySQL', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg', color: 'from-blue-600 to-blue-400' },
-        { name: 'PostgreSQL', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg', color: 'from-blue-700 to-blue-500' },
-        { name: 'Supabase', src: 'https://raw.githubusercontent.com/supabase/supabase/master/packages/common/assets/images/supabase-logo-icon.svg', color: 'from-green-600 to-emerald-500' },
-        { name: 'Firebase', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg', color: 'from-yellow-500 to-orange-500' },
-        { name: 'Ghost', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ghost/ghost-original.svg', color: 'from-gray-800 to-gray-600' },
-        { name: 'Wordpress', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-original.svg', color: 'from-gray-800 to-gray-600' },
-      ]
-    },
-    {
-      category: 'DevOps & Tools',
-      technologies: [
-        { name: 'Git', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg', color: 'from-orange-500 to-red-400' },
-        { name: 'GitHub', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg', color: 'from-gray-800 to-gray-600' },
-        { name: 'Vercel', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vercel/vercel-original.svg', color: 'from-gray-900 to-gray-700' },
-        { name: 'Netlify', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/netlify/netlify-original.svg', color: 'from-teal-400 to-cyan-400' },
-        { name: 'Docker', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg', color: 'from-sky-400 to-blue-400' },
+        {
+          name: "MySQL",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+          color: "from-blue-600 to-blue-400",
+        },
+        {
+          name: "PostgreSQL",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+          color: "from-blue-700 to-blue-500",
+        },
+        {
+          name: "Supabase",
+          src: "https://raw.githubusercontent.com/supabase/supabase/master/packages/common/assets/images/supabase-logo-icon.svg",
+          color: "from-green-600 to-emerald-500",
+        },
+        {
+          name: "Firebase",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
+          color: "from-yellow-500 to-orange-500",
+        },
+        {
+          name: "Vercel",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vercel/vercel-original.svg",
+          color: "from-gray-900 to-gray-700",
+        },
+        {
+          name: "Netlify",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/netlify/netlify-original.svg",
+          color: "from-teal-400 to-cyan-400",
+        },
+        {
+          name: "Docker",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+          color: "from-sky-400 to-blue-400",
+        },
+        {
+          name: "Wordpress",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-original.svg",
+          color: "from-gray-800 to-gray-600",
+        },
+        {
+          name: "Winbox",
+          src: "/winbox.png",
+          color: "from-blue-500 to-sky-500",
+        },
+        {
+          name: "VirtualBox",
+          src: "/vmware-1.png",
+          color: "from-blue-600 to-orange-500",
+        },
+        {
+          name: "Kali Linux",
+          src: "https://www.kali.org/images/favicon.png",
+          color: "from-gray-800 to-blue-500",
+        },
+        {
+          name: "Git",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+          color: "from-orange-500 to-red-400",
+        },
       ],
     },
     {
-      category: 'AI & Automation',
-      technologies: [
-        { name: 'Python', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg', color: 'from-yellow-400 to-blue-400' },
-        { name: 'TensorFlow', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tensorflow/tensorflow-original.svg', color: 'from-orange-400 to-yellow-400' },
-        { name: 'Pytorch', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original.svg', color: 'from-red-500 to-orange-400' },
-        { name: 'ROS', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ros/ros-original.svg', color: 'from-indigo-500 to-indigo-300' },
-         { name: 'N8n', src: '/nnn-color.png',
-      color: 'from-pink-500 to-pink-300',
-    },
-      ],
-    },
-    {
-  category: 'UI/UX & Design',
-  technologies: [
-    {
-      name: 'Figma',
-      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg',
-      color: 'from-purple-500 to-pink-400',
-    },
-    {
-      name: 'Framer',
-      src: '/framer-Icon.png',
-      color: 'from-gray-500 to-gray-400',
-    },
-    {
-      name: 'Adobe Photoshop',
-      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-original.svg',
-      color: 'from-blue-600 to-cyan-400',
-    },
-    {
-      name: 'Adobe Illustrator',
-      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/illustrator/illustrator-original.svg',
-      color: 'from-orange-500 to-red-400',
-    },
-    {
-      name: 'Canva',
-      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg',
-      color: 'from-blue-500 to-teal-400',
-    },
-    {
-      name: 'LottieFiles',
-      src: '/lottiefiles.png',
-      color: 'from-blue-400 to-indigo-400',
-    },
-    {
-      name: 'Notion',
-      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/notion/notion-original.svg',
-      color: 'from-gray-700 to-gray-500',
-    },
-    {
-      name: 'Google Fonts',
-      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg',
-      color: 'from-red-500 to-yellow-500',
-    },
-    {
-      name: 'Coolors',
-      src: '/coolors.png',
-      color: 'from-purple-600 to-purple-400',
-    },
-    {
-      name: 'MockFlow',
-      src: '/mockflow-icon-1.png',
-      color: 'from-blue-700 to-blue-500',
-    },
-        {
-      name: 'Spline',
-      src: '/spline-1.png',
-      color: 'from-pink-500 to-orange-400',
-    },
-        {
-      name: 'Webflow',
-      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webflow/webflow-original.svg',
-      color: 'from-indigo-400 to-blue-400',
-    },
-  ],
-},
-    {
-      category: 'Other Tools',
+      category: "UI/UX & Design",
       technologies: [
         {
-  name: 'Winbox',
-  src: '/winbox.png',
-  color: 'from-blue-500 to-sky-500',
-},
-{
-  name: 'VirtualBox',
-  src: '/vmware-1.png',
-  color: 'from-blue-600 to-orange-500',
-},
-{
-  name: 'Kali Linux',
-  src: 'https://www.kali.org/images/favicon.png',
-  color: 'from-gray-800 to-blue-500',
-},
-{
-  name: 'Arduino IDE',
-  src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original.svg',
-  color: 'from-teal-500 to-green-400',
-},
-        { name: 'Markdown', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg', color: 'from-gray-700 to-gray-500' },
-        { name: 'Linux', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg', color: 'from-yellow-400 to-orange-400' },
-        { name: 'Yarn', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/yarn/yarn-original.svg',
-      color: 'from-sky-500 to-blue-500',
-    },
-        { name: 'npm',src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg',
-      color: 'from-red-500 to-pink-500',
-    },
-            {
-      name: 'Nginx',
-      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg',
-      color: 'from-green-500 to-emerald-500',
-    },
-    {
-      name: 'Bash',
-      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg',
-      color: 'from-gray-700 to-gray-500',
-    },
-        
+          name: "Figma",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
+          color: "from-purple-500 to-pink-400",
+        },
+        {
+          name: "Framer",
+          src: "/framer-Icon.png",
+          color: "from-gray-500 to-gray-400",
+        },
+        {
+          name: "Adobe Photoshop",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-original.svg",
+          color: "from-blue-600 to-cyan-400",
+        },
+        {
+          name: "Adobe Illustrator",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/illustrator/illustrator-original.svg",
+          color: "from-orange-500 to-red-400",
+        },
+        {
+          name: "Canva",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/canva/canva-original.svg",
+          color: "from-blue-500 to-teal-400",
+        },
+        {
+          name: "LottieFiles",
+          src: "/lottiefiles.png",
+          color: "from-blue-400 to-indigo-400",
+        },
+        {
+          name: "Notion",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/notion/notion-original.svg",
+          color: "from-gray-700 to-gray-500",
+        },
+        {
+          name: "Google Fonts",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg",
+          color: "from-red-500 to-yellow-500",
+        },
+        {
+          name: "Coolors",
+          src: "/coolors.png",
+          color: "from-purple-600 to-purple-400",
+        },
+        {
+          name: "MockFlow",
+          src: "/mockflow-icon-1.png",
+          color: "from-blue-700 to-blue-500",
+        },
+        {
+          name: "Spline",
+          src: "/spline-1.png",
+          color: "from-pink-500 to-orange-400",
+        },
+        {
+          name: "Webflow",
+          src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webflow/webflow-original.svg",
+          color: "from-indigo-400 to-blue-400",
+        },
       ],
     },
   ];
@@ -205,7 +246,9 @@ const TechStack = () => {
             Tech <span className="gradient-text">Stack</span>
           </h2>
           <p className="text-lg sm:text-xl text-gray-300 max-w-3xl mx-auto font-poppins px-4 sm:px-0">
-            Technologies and tools I use to build intelligent systems, from frontend interfaces to AI-powered automation and IoT hardware integration
+            Technologies and tools I use to build intelligent systems, from
+            frontend interfaces to AI-powered automation and IoT hardware
+            integration
           </p>
         </motion.div>
 
@@ -243,14 +286,18 @@ const TechStack = () => {
                     className="group cursor-pointer"
                   >
                     <div className="relative bg-black/40 backdrop-blur-sm border border-white/10 rounded-xl p-4 sm:p-6 text-center hover:border-white/20 transition-all duration-300 h-full flex flex-col items-center justify-center min-h-[120px] sm:min-h-[140px]">
-                      <div className={`absolute inset-0 bg-gradient-to-br ${tech.color} opacity-0 group-hover:opacity-10 rounded-xl transition-opacity duration-300`}></div>
+                      <div
+                        className={`absolute inset-0 bg-gradient-to-br ${tech.color} opacity-0 group-hover:opacity-10 rounded-xl transition-opacity duration-300`}
+                      ></div>
                       <div className="relative z-10 text-2xl sm:text-3xl mb-2 sm:mb-3 group-hover:scale-110 transition-transform duration-300">
                         <TechIcon src={tech.src} name={tech.name} />
                       </div>
                       <h4 className="relative z-10 font-poppins text-xs sm:text-sm font-semibold text-white group-hover:text-transparent group-hover:bg-gradient-to-r group-hover:bg-clip-text group-hover:from-white group-hover:to-gray-300 transition-all duration-300 text-center leading-tight">
                         {tech.name}
                       </h4>
-                      <div className={`absolute inset-0 rounded-xl opacity-0 group-hover:opacity-20 bg-gradient-to-br ${tech.color} blur-xl transition-opacity duration-300 -z-10`}></div>
+                      <div
+                        className={`absolute inset-0 rounded-xl opacity-0 group-hover:opacity-20 bg-gradient-to-br ${tech.color} blur-xl transition-opacity duration-300 -z-10`}
+                      ></div>
                     </div>
                   </motion.div>
                 ))}
@@ -258,7 +305,6 @@ const TechStack = () => {
             </motion.div>
           ))}
         </div>
-
       </div>
     </section>
   );
